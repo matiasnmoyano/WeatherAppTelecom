572336025dfe1f8ba6d4ce05ee2d469334e51ff1
matiasnmoyano <moyano.matiasnicolas.mm@gmail.com>
2022-02-02 19:28:39 -0300

    diseño general

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import DateCountry from './components/DateCountry';
+import SelectCountries from './components/SelecCountries';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="section">
+        <SelectCountries/>
+      </section>
+      <main className="main">
+        <DateCountry/>
+      </main>
     </div>
   );
 }
---
+++ my-app/src/components/SelecCountries/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles.css'
+import {Paper} from "@mui/material"
+export default function SelectCountries () {
+    return (
+        <Paper className="paper" elevation={12}>
+            Holass
+        </Paper>
+    )
+}
